e38a708d741a4d586de48357528ef5425cbebd35
Braylon Li <270527452+Braylonli-dev@users.noreply.github.com>
2026-06-25 00:52:07 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for(int i = 0; i < nums.size(); i++){
+            int x = target - nums[i];
+            if(hash.find(x) != hash.end()){
+                return {hash[x], i};
+            }
+            hash[nums[i]] = i;
+        }
+    return {};
+    }
+};
